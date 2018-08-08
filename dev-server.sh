24309f99829c7578e9c4eda4b0a4857451accc7c
Emmanuel Okorie <eokorie@ukpos.com>
2018-08-08 13:22:00 +0100

    Added a tweak to rebuild docker files

--- dev-server.sh
+++ dev-server.sh
@@ -5,11 +5,11 @@ case "$1" in
         if [ "$2" == "apache2" ]
         then
             printf "Starting $2 server......"
-            docker-compose up -d apache2 redis portainer beanstalkd beanstalkd-console mariadb phpmyadmin workspace
+            docker-compose up -d apache2 redis portainer beanstalkd beanstalkd-console mariadb phpmyadmin workspace php-worker
         elif [ "$2" == "nginx" ] 
         then
             printf "Starting $2 server......"
-            docker-compose up -d nginx redis portainer beanstalkd beanstalkd-console mariadb phpmyadmin workspace
+            docker-compose up -d nginx redis portainer beanstalkd beanstalkd-console mariadb phpmyadmin workspace php-worker
         else
             echo 'Selected server not found.  Choices are apache2 or nginx'
         fi
@@ -20,14 +20,16 @@ case "$1" in
     stop)
         docker-compose stop
         ;;
+    build)
+        docker-compose build apache2 nginx redis portainer beanstalkd beanstalkd-console mariadb phpmyadmin workspace php-worker
+        ;;
+    rebuild)
+        docker-compose stop && \
+        docker-compose rm -y && \
+        docker-compose pull && \
+        docker-compose build --no-cache apache2 nginx redis portainer beanstalkd beanstalkd-console mariadb phpmyadmin workspace php-worker
+        ;;
     *)
         echo don\'t know
         ;;
 esac
-
-
-
-
-
-
-
